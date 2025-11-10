f3ec87a694c1d05a10ca4198e1c824f88cbe27a6
José Antonio Ruiz Heredia <56451051+JoseRuiz01@users.noreply.github.com>
2025-11-10 11:55:52 +0100

    resnet50

--- src/training/train.py
+++ src/training/train.py
@@ -98,6 +98,17 @@ def train_model(
                 param.requires_grad = False
 
         model.fc = nn.Linear(model.fc.in_features, num_classes)
+        
+    elif model_name.lower() == "resnet50":
+        model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
+        
+
+        if freeze_backbone:
+            for param in model.parameters():
+                param.requires_grad = False
+                
+        model.fc = nn.Linear(model.fc.in_features, num_classes)
+        
     else:
         raise ValueError(f"Unsupported model: {model_name}")
 
